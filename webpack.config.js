25e96b56deeba02b2cdf34399c77f275f7e6da1f
shema1 <shymechko.m.a@gmail.com>
2020-05-14 21:35:32 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require("webpack");
 
 module.exports = (env, argv) => {
