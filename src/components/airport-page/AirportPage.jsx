4b2e7bb3a551e7b70194b3ab6208e62c9f696ca5
shema1 <shymechko.m.a@gmail.com>
2020-05-18 13:24:57 +0300

    commit

--- src/components/airport-page/AirportPage.jsx
+++ src/components/airport-page/AirportPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import SearchForm from '../search-form/SearchForm'
-import FlightsSchedule from '../flights-schedule/flightsSchedule'
+import FlightsSchedule from '../flights-schedule/FlightsSchedule'
 
 const AirportPage = () => {
 	return (
 		<>
 			<SearchForm />
-			<FlightsSchedule />
+			<FlightsSchedule/>
 		</>
 	)
 }
